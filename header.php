f5a732470a200d35da9b783cdc028a235b1a5ef2
Imon <contact@imonweb.co.uk>
2023-12-24 00:30:10 +0000

    notes

--- header.php
+++ header.php
@@ -32,6 +32,9 @@
         <div class="site-header__util">
 
           <?php if(is_user_logged_in()){ ?>
+
+             <a href="<?php echo esc_url(site_url('/my-notes')); ?>" class="btn btn--small btn--orange float-left push-right">My Notes</a>
+
              <a href="<?php echo wp_logout_url(); ?>" class="btn btn--small  btn--dark-orange float-left btn--with-photo">
              <!-- Log Out -->
              <span class="site-header__avatar"><?php echo get_avatar(get_current_user_id(), 60); ?></span>
