ca429f3bc432ff307da9caa15b66f89a79b80660
Imon <contact@imonweb.co.uk>
2023-12-29 13:06:52 +0000

    add mu-plugin into theme folder for backup

---
+++ university-post-types-backup.php
@@ -0,0 +1,121 @@
+<?php 
+
+function university_post_types(){
+
+   // Campus Post Type
+  register_post_type('campus', array(
+    'capability_type' => 'campus',
+    'map_meta_cap' => true,
+
+    'supports' => array('title', 'editor', 'excerpt'),
+    'show_in_rest' => true,
+    'rewrite'       => array('slug' => 'campuses'),
+    'has_archive'   => true,
+    'public'        => true,
+    'show_in_rest'  => true,
+    'labels'        => array(
+    'name'          => 'Campuses',
+    'add_new'  => 'Add New Campus',
+    'edit_item'     => 'Edit Campus',
+    'all_items'     => 'All Campuses',
+    'singular_name' => 'Campus'
+    ),
+    'menu_icon'     => 'dashicons-location-alt'
+  ));
+
+  // Event Post Type
+  register_post_type('event', array(
+    /*  Event Planner - member */
+    'capability_type' => 'event',
+    'map_meta_cap' => true,
+    
+    'supports' => array('title', 'editor', 'excerpt'),
+    'show_in_rest' => true,
+    'rewrite'       => array('slug' => 'events'),
+    'has_archive'   => true,
+    'public'        => true,
+    'show_in_rest'  => true,
+    'labels'        => array(
+    'name'          => 'Events',
+    'add_new'  => 'Add New Event',
+    'edit_item'     => 'Edit Event',
+    'all_items'     => 'All Events',
+    'singular_name' => 'Event'
+    ),
+    'menu_icon'     => 'dashicons-calendar'
+  ));
+
+  // Program Post Type
+  register_post_type('program', array(
+    'show_in_rest' => true,
+    'supports' => array('title'),
+    'rewrite' => array('slug' => 'programs'),
+    'has_archive' => true,
+    'public' => true,
+    'labels' => array(
+      'name' => 'Programs',
+      'add_new' => 'Add New Program',
+      'edit_item' => 'Edit Program',
+      'all_items' => 'All Programs',
+      'singular_name' => 'Program'
+    ),
+    'menu_icon' => 'dashicons-awards'
+  ));
+
+  // Professor Post Type
+  register_post_type('professor', array(
+    'show_in_rest' => true,
+    'supports' => array('title', 'editor', 'thumbnail'),
+    'public' => true,
+    'labels' => array(
+      'name' => 'Professors',
+      'add_new' => 'Add New Professor',
+      'edit_item' => 'Edit Professor',
+      'all_items' => 'All Professors',
+      'singular_name' => 'Professor'
+    ),
+    'menu_icon' => 'dashicons-welcome-learn-more'
+  ));
+
+   // Note Post Type
+  register_post_type('note', array(
+    // allow subscriber to create note post 
+    'capability_type' => 'note',
+    'map_meta_cap' => true,
+
+    'show_in_rest' => true,
+    'supports' => array('title', 'editor'),
+    'public' => false,
+    // public post type setting to false will hide in the admin dashboard.
+    // set show_ui to true 
+    'show_ui' => true,
+    'labels' => array(
+      'name' => 'Notes',
+      // old
+      // 'add_new_item' => 'Add New Note',
+      'add_new' => 'Add New Note',
+      'edit_item' => 'Edit Note',
+      'all_items' => 'All Notes',
+      'singular_name' => 'Note'
+    ),
+    'menu_icon' => 'dashicons-welcome-write-blog'
+  ));
+
+  // Like Post Type
+  register_post_type('like', array(
+    'supports' => array('title'),
+    'public' => false,
+    'show_ui' => true,
+    'labels' => array(
+      'name' => 'Likes',
+      'add_new' => 'Add New Like',
+      'edit_item' => 'Edit Like',
+      'all_items' => 'All Likes',
+      'singular_name' => 'Like'
+    ),
+    'menu_icon' => 'dashicons-heart'
+  ));
+
+} // university post types
+
+add_action('init', 'university_post_types');
